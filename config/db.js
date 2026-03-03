84fbd9b23163fe4ae889a649a6104b687d9f3f90
psc981 <teamkhan584@gmail.com>
2026-03-03 14:40:48 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -6,7 +6,9 @@ const connectDB = async () => {
   if (isConnected) return; // use existing connection
 
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "usdt_store",
+    });
     console.log("MONGO_URI:", process.env.MONGO_URI.replace(/:.+@/, ":****@"));
     isConnected = conn.connections[0].readyState === 1;
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
